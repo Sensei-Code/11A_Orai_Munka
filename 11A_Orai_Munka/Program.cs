6456cfa988ab7f3ff75e1baefca865e79cae8603
Tóth Lajos <lajos.toth@windowslive.com>
2020-01-29 10:48:00 +0100

    Add project files.

---
+++ 11A_Orai_Munka/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11A_Orai_Munka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
